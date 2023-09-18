16fb9429d96c55bcec626f111ee4f6cd7fb8136e
Md Siam Maruf <siam.maruf1@gmail.com>
2023-09-19 03:55:16 +0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const url = require("url");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser());
+
+let clientResponseRef;
+
+app.get('/*', (req, res)=>{
+    const pathName = url.parse(req.url).pathname;
+    const obj = {
+        pathname: pathName,
+        method: "get",
+        params: req.query
+    }
+
+    io.emit("page-request", obj);
+    clientResponseRef = res;
+});
+
+app.post('/*', (req, res)=>{
+    const pathName = url.parse(req.url).pathname;
+    const obj = {
+        pathname: pathName,
+        method: "post",
+        params: req.body
+    }
+
+    io.emit("page-request", obj);
+    clientResponseRef = res;
+});
+
+io.on("connection", (socket)=>{
+    console.log("A server connected !");
+    socket.on("page-response",(response)=>{
+        clientResponseRef.send(response);
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT,()=>{
+    console.log("Listening on :"+ PORT);
+})
