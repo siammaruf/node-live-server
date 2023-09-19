ccdb8093b87d9871dcf23dc281f6b6b417f24ac4
Md Siam Maruf <siam.maruf1@gmail.com>
2023-09-19 10:50:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -12,35 +12,35 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
   });
 
-app.get('/*', (req, res)=>{
-    const pathName = url.parse(req.url).pathname;
-    const obj = {
-        pathname: pathName,
-        method: "get",
-        params: req.query
-    }
+// app.get('/*', (req, res)=>{
+//     const pathName = url.parse(req.url).pathname;
+//     const obj = {
+//         pathname: pathName,
+//         method: "get",
+//         params: req.query
+//     }
 
-    io.emit("page-request", obj);
-    clientResponseRef = res;
-});
+//     io.emit("page-request", obj);
+//     clientResponseRef = res;
+// });
 
-app.post('/*', (req, res)=>{
-    const pathName = url.parse(req.url).pathname;
-    const obj = {
-        pathname: pathName,
-        method: "post",
-        params: req.body
-    }
+// app.post('/*', (req, res)=>{
+//     const pathName = url.parse(req.url).pathname;
+//     const obj = {
+//         pathname: pathName,
+//         method: "post",
+//         params: req.body
+//     }
 
-    io.emit("page-request", obj);
-    clientResponseRef = res;
-});
+//     io.emit("page-request", obj);
+//     clientResponseRef = res;
+// });
 
 io.on("connection", (socket)=>{
     console.log("A server connected !");
-    socket.on("page-response",(response)=>{
-        clientResponseRef.send(response);
-    })
+    // socket.on("page-response",(response)=>{
+    //     clientResponseRef.send(response);
+    // })
 })
 
 const PORT = process.env.PORT || 3000;
